e146dd945c6eca7df0161227c9dfc15559fbf422
FDen <den9816@yandex.ru>
2019-05-19 16:20:27 +0300

    Урок 3

---
+++ lesson3/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// let budgetMonth = prompt("Ваш бюджет на месяц?");
+// let dateTime = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budgetMonth: prompt("Ваш бюджет на месяц?"),
+    timeData: prompt("Введите дату в формате YYYY-MM-DD"),
+    expences : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько обойдётся?");
+
+    appData.expences[a] = b;
+    console.log(appData.expences[a]);
+}
+
+alert(dayBudget(appData.budgetMonth));
+
+function dayBudget(budgetMonth){
+    return (budgetMonth / 30);
+}
