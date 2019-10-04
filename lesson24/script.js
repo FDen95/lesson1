f7c6296cc19f669a4e8715996ae02f54d1dd71c7
Denis <dsf@resortsoft.ru>
2019-10-04 17:30:30 +0300

    var, let, const

--- lesson24/script.js
+++ lesson24/script.js
@@ -3,3 +3,22 @@ let name = "Ivan",
     mail = 'ex@mail.ru';
 
 document.write(`Пользователю ${name} ${age} лет`);
+
+function makeArray () {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+
+    return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[4]();
+arr[8]();
